1ac674ed156332a8487d9cb42c7e173eedf25577
Gamer_Z <grasmanek94@users.noreply.github.com>
2015-05-23 21:22:49 +0200

    fix infinite loading bug / 500 error

--- dl.php
+++ dl.php
@@ -1,14 +1,17 @@
 <?php
     $basedirectory = './.uploads/' . str_replace(".", "", $_SERVER['QUERY_STRING']) . "/files/";
     $h = opendir($basedirectory); //Open the current directory
-    while (false !== ($entry = readdir($h))) 
+    if(file_exists($basedirectory))
     {
-        if($entry != '.' && $entry != '..' && is_file($basedirectory . $entry))//Skips over . and .. and directories
-        { 
-            header("Content-Type: application/force-download");
-            header('Content-Disposition: attachment; filename="'.$entry.'"');
-            readfile($basedirectory . $entry);            
-            break; //Exit the loop so no more files are read
+        while (false !== ($entry = readdir($h))) 
+        {
+            if($entry != '.' && $entry != '..' && is_file($basedirectory . $entry))//Skips over . and .. and directories
+            { 
+                header("Content-Type: application/force-download");
+                header('Content-Disposition: attachment; filename="'.$entry.'"');
+                readfile($basedirectory . $entry);            
+                break; //Exit the loop so no more files are read
+            }
         }
     }
     header("HTTP/1.0 404 Not Found");
